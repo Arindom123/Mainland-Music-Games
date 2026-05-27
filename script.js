6a1a7c0dc655ec144a773dd5b64e87c98965f9ff
Arindom123 <arindom.paul123@gmail.com>
2026-05-27 18:22:17 -0400

    editing ui features

--- script.js
+++ script.js
@@ -2,37 +2,41 @@ const homeDiv = document.getElementById("home");
 const playDiv = document.getElementById("play");
 const instrumentsDiv = document.getElementById("instruments");
 const instrumentButtons = instrumentsDiv.querySelectorAll('button');
-const scaleDiv = document.getElementById("scales");
-const scaleButtons = scaleDiv.querySelectorAll('button');
+const scaleSelectionDiv = document.getElementById("scaleSelection");
+const playingDiv = document.getElementById("playing");
+const scaleButtons = scaleSelectionDiv.querySelectorAll('button');
 const homeButton = document.getElementById("home-logo");
 const rootDropdown = document.getElementById("root");
 const startButton = document.getElementById("startButton");
+const backButton = document.getElementById("backButton");
+const scaleNameText = document.getElementById("scaleName");
 
 playDiv.classList.add('hidden');
+playingDiv.classList.add('hidden');
 
 const scalePatternsMap = new Map();
-scalePatternsMap.set("majorIonianPattern", [2, 2, 1, 2, 2, 2, 1]); //major
-scalePatternsMap.set("majorLydianPattern", [2, 2, 2, 1, 2, 2, 1]);
-scalePatternsMap.set("majorMixolydianPattern", [2, 2, 1, 2, 2, 1, 2]);
-scalePatternsMap.set("majorHarmonicPattern", [2, 2, 1, 2, 1, 3, 1]);
-scalePatternsMap.set("majorPentatonicPattern", [2, 2, 3, 2, 3]);
-scalePatternsMap.set("majorBluesPattern", [2, 1, 1, 3, 2, 3]);
-scalePatternsMap.set("minorAeolianPattern", [2, 1, 2, 2, 1, 2, 2]); //minor
-scalePatternsMap.set("minorDorianPattern", [2, 1, 2, 2, 2, 1, 2]);
-scalePatternsMap.set("minorPhrygianPattern", [1, 2, 2, 2, 1, 2, 2]);
-scalePatternsMap.set("minorHarmonicPattern", [2, 1, 2, 2, 1, 3, 1]);
-scalePatternsMap.set("minorMelodicPattern", [2, 1, 2, 2, 2, 2, 1]);
-scalePatternsMap.set("minorPentatonicPattern", [3, 2, 2, 3, 2]);
-scalePatternsMap.set("minorBluesPattern", [3, 2, 1, 1, 3, 2]);
-scalePatternsMap.set("chromaticPattern", [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]); //other
-scalePatternsMap.set("locrianPattern", [1, 2, 2, 1, 2, 2, 2]);
+scalePatternsMap.set("majorIonian", [2, 2, 1, 2, 2, 2, 1]); //major
+scalePatternsMap.set("majorLydian", [2, 2, 2, 1, 2, 2, 1]);
+scalePatternsMap.set("majorMixolydian", [2, 2, 1, 2, 2, 1, 2]);
+scalePatternsMap.set("majorHarmonic", [2, 2, 1, 2, 1, 3, 1]);
+scalePatternsMap.set("majorPentatonic", [2, 2, 3, 2, 3]);
+scalePatternsMap.set("majorBlues", [2, 1, 1, 3, 2, 3]);
+scalePatternsMap.set("minorAeolian", [2, 1, 2, 2, 1, 2, 2]); //minor
+scalePatternsMap.set("minorDorian", [2, 1, 2, 2, 2, 1, 2]);
+scalePatternsMap.set("minorPhrygian", [1, 2, 2, 2, 1, 2, 2]);
+scalePatternsMap.set("minorHarmonic", [2, 1, 2, 2, 1, 3, 1]);
+scalePatternsMap.set("minorMelodic", [2, 1, 2, 2, 2, 2, 1]);
+scalePatternsMap.set("minorPentatonic", [3, 2, 2, 3, 2]);
+scalePatternsMap.set("minorBlues", [3, 2, 1, 1, 3, 2]);
+scalePatternsMap.set("chromatic", [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]); //other
+scalePatternsMap.set("locrian", [1, 2, 2, 1, 2, 2, 2]);
 
 const chromaticScale = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb","B"];
 const masterScale = [
     ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb","B"], 
     ["C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2","B2"],
     ["C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3","B3"],
-//    ["C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4","B4"]
+//  ["C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4","B4"]
 ];
 
 let currentScale = [];
@@ -40,6 +44,9 @@ let scalePattern = [];
 let root = rootDropdown.value;
 let naturalRungDiv = document.getElementById("naturalRungs");
 let accidentalRungDiv = document.getElementById("accidentalRungs");
+let play = null;
+let currentIndex = 0;
+let scaleName = "";
 
 rootDropdown.addEventListener("change", function()
 {
@@ -54,8 +61,21 @@ homeButton.addEventListener("click", function()
 
 startButton.addEventListener("click", function()
 {
-    scaleDiv.classList.add('hidden');
+    clearInterval(play);
+    document.querySelectorAll('.selected').forEach(element => element.classList.remove('selected'));
+    scaleSelectionDiv.classList.add('hidden');
+    playingDiv.classList.remove('hidden');
     playScale(root, scalePattern, 2);
+    startButton.textContent = "Replay";
+});
+
+backButton.addEventListener("click", function()
+{
+    clearInterval(play);
+    document.querySelectorAll('.selected').forEach(element => element.classList.remove('selected'));
+    scaleSelectionDiv.classList.remove('hidden');
+    startButton.textContent = "Play";
+    playingDiv.classList.add('hidden');
 });
 
 scaleButtons.forEach(instantiateScaleButtons);
@@ -72,8 +92,13 @@ function instantiateScaleButtons (scaleButton)
 {
     scaleButtons.forEach(enableButton);
     scaleButton.disabled = true;
-    scalePattern = scalePatternsMap.get(scaleButton.getAttribute('data-pattern'));
-    console.log(scalePattern);
+    let scaleNameInitial = scaleButton.getAttribute('data-pattern')
+    scalePattern = scalePatternsMap.get(scaleNameInitial);
+    scaleName = scaleNameInitial.charAt(0).toUpperCase() + scaleNameInitial.slice(1);
+    if (scaleName.includes("Major") || scaleName.includes("Minor"))
+    {
+    scaleName = scaleName.slice(0, 5) + " " + scaleName.slice(5);
+    }
 });
 }
 
@@ -117,20 +142,24 @@ function fillRungs () {
 function playScale (root, scalePattern, octaves)
 {
     const scaleAscending = generateScale(root, scalePattern, octaves);
+    scaleNameText.textContent = "Scale: " + root + " " + scaleName;
     console.log(scaleAscending);
     const scaleDescending = scaleAscending.slice(0,-1).toReversed();
     const scale = scaleAscending.concat(scaleDescending);
-    let currentIndex = 0;
-    let play = setInterval(() => {
-        if (currentIndex == scale.length)
-        {
-            clearInterval(play);
-            document.querySelectorAll('.selected').forEach(element => element.classList.remove('selected'));
-            return;
-        }
-        masterScaleIndexer(scale[currentIndex], currentIndex);
-        currentIndex++;
-    }, 500);
+    currentIndex = 0;
+    play = setInterval(function() {interval(scale)}, 500);
+}
+
+function interval (scale)
+{
+    if (currentIndex == scale.length)
+    {
+        clearInterval(play);
+        document.querySelectorAll('.selected').forEach(element => element.classList.remove('selected'));
+        return;
+    }
+    masterScaleIndexer(scale[currentIndex], currentIndex);
+    currentIndex++;
 }
 
 function masterScaleIndexer (note, i)
